38631fa09bf8ac6366f123b40dc450fe3fbee9de
Jakub-Ciecierski <jakub.ciecier@gmail.com>
2016-12-06 16:36:15 +0100

    Project structure refactore, Configuration file

---
+++ modules/common/resources/src/resources/resources.cpp
@@ -0,0 +1,52 @@
+#include "resources/resources.h"
+#include <root_directory.h> // cmake configuration
+
+namespace ifx {
+
+Resources::Resources(){
+    InitResourcePath();
+}
+
+void Resources::InitResourcePath(){
+    resource_root_path_ = ifx_root;
+    resource_root_path_ += "/res";
+}
+
+std::string Resources::ResouceTypePath(ResourceType type){
+    switch(type){
+        case ResourceType::MODEL:
+            return "models";
+        case ResourceType::SHADER:
+            return "shaders";
+        case ResourceType::TEXTURE:
+            return "textures";
+    }
+    return "unknown";
+}
+
+std::string Resources::ConcatenatePath(std::string path1, std::string path2){
+    return path1 + "/" + path2;
+}
+
+Resources::~Resources(){
+
+}
+
+Resources& Resources::GetInstance(){
+    static Resources resources;
+
+    return resources;
+}
+
+std::string Resources::GetResourcePath(std::string name, ResourceType type){
+    std::string resource_type_path = ResouceTypePath(type);
+    std::string resource_type_full_path = ConcatenatePath(resource_root_path_,
+                                                          resource_type_path);
+
+    std::string resource_full_path = ConcatenatePath(resource_type_full_path,
+                                                     name);
+
+    return resource_full_path;
+}
+
+}
