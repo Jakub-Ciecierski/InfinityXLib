42fd462be8af5b104b15c62dc87fcd4a94284b50
Jakub-Ciecierski <jakub.ciecier@gmail.com>
2017-12-14 22:51:34 +0100

    Meshing Parameters

--- modules/gui/editor/src/editor/views/soft_body_views/meshing/soft_body_meshing_settings_view.cpp
+++ modules/gui/editor/src/editor/views/soft_body_views/meshing/soft_body_meshing_settings_view.cpp
@@ -10,9 +10,17 @@ void SoftBodyMeshingSettingsView::Render() {
     if (ImGui::TreeNodeEx("Parameters",
                           ImGuiTreeNodeFlags_DefaultOpen)) {
         ImGui::PushItemWidth(50);
-        ImGui::InputFloat("Maximum Volume",
-                           &rtfem_options_.maximum_volume);
+        ImGui::InputFloat("Max Volume",
+                          &rtfem_options_.maximum_volume);
+        ImGui::InputFloat("Max Radius-Edge Ratio",
+                          &rtfem_options_.max_radius_edge_ratio);
         ImGui::PopItemWidth();
+
+        ImGui::PushItemWidth(100);
+        ImGui::InputInt("Min Dihedral Angle",
+                        &rtfem_options_.min_dihedral_angle_degree);
+        ImGui::PopItemWidth();
+
         ImGui::TreePop();
     }
 }
